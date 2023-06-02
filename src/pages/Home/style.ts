3429e689a389e9c55aaaab47738e973a7b411cb7
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-06-01 22:59:41 -0300

    :up:

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -12,13 +12,6 @@ export const HomeContainer = styled.main`
     align-items: center;
     gap: 3.5rem;
   }
-
-  input {
-    background: ${(props) => props.theme['gray-400']};
-    color: ${(props) => props.theme['gray-100']};
-    border: none;
-    gap: 2rem;
-  }
 `
 
 export const FormContainer = styled.div`
@@ -56,4 +49,49 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  color: ${(props) => props.theme['gray-100']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+export const MinutesAmountInput = styled(BaseInput)`
+    width: 4rem;
+`
+
+export const StartCountDownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
